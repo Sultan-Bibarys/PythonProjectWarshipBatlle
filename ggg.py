6c5ff88da4fc0242e5b9f709cdca47c2203d062b
Sultan-Bibarys <57250403+Sultan-Bibarys@users.noreply.github.com>
2019-12-02 22:31:44 +0600

    Add files via upload

---
+++ ggg.py
@@ -0,0 +1,13 @@
+class ggg():
+    _name='n/a'
+    _age=0
+
+    def set_name(self,n):
+        self._name=n
+    def set_age(self,a):
+        self._age=a
+    def description_of_person(self):
+        print('---------------------------------')
+        print('my name is',self._name)
+        print("I am" ,self._age,"years old")
+        
